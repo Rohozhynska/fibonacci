86b291f134f585b937fe13eb5e7e8c4aa6e340ba
Natalya Rohozhynska <rohozhinska@gmail.com>
2018-10-02 07:02:10 +0000

    g

--- js/index.js
+++ js/index.js
@@ -1,37 +1,9 @@
-let num = parseFloat(prompt('Enter a number'))
+let num = parseFloat(prompt('Enter a number'));
 let numFibb = [1, 1];
-
-
-for (let k = 2 ; k>0;) {
-    
-    let r = numFibb[numFibb.length - 2]
-    let k = numFibb[numFibb.length - 1]
-    let m = r + k
+for (let i = 2; i < num; i++) {
+    let r = numFibb[numFibb.length - 2];
+    let k = numFibb[numFibb.length - 1];
+    let m = r + k;
     numFibb.push(m);
-    if (k<num) { continue
-    }
-    else{
-       document.write(m)
-    }
-    
 }
-    
-//alert(numFibb[numFibb.length-1])
-    
-    // {numFibb.push(m)
-    // if (m<num) {continue}
-
-    //     alert(m)}
-
-
-    // r=numFibb.length;
-    // let k=0+numFibb[0]
-    // numFibb.push(k)
-    //     let m=k+numFibb[0];
-    //     numFibb.push(m)
-    // do {  k=k+m;;
-    // numFibb.push(k)
-    // let r=r+k
-    // } while (numFibb.length==num)
-    // function count(numFibb) {
-    // }
+alert(numFibb[numFibb.length - 1]);
